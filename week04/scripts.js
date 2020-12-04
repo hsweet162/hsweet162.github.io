9b710f87b4753a7353347c40c68e08a6cadad859
Sevelen Jaimin <hsweet162@gmail.com>
2020-12-04 21:31:06 +0800

    2131

--- week04/scripts.js
+++ week04/scripts.js
@@ -32,3 +32,10 @@ elPassword2.addEventListener("blur", function(){
     let a = document.getElementById("kataLaluan2");
     checkPasswordLength2(a.value);
 });
+
+let metallicaLink = document.getElementById("metallicaLink";
+metallicaLink.addEventListener("click", function(){
+    event.preventDefault();
+    alert(metallicaLink.href);
+
+})
