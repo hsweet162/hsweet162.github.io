83298162e9c5d03d9d2527d504128aba1cc8d2d2
Sevelen Jaimin <hsweet162@gmail.com>
2020-12-04 20:04:35 +0800

    2004

---
+++ week04/scripts.js
@@ -0,0 +1,7 @@
+function transformUppercase(){
+    let a = document.getElementById("namaPengguna");
+    a.value = a.value.toUpperCase();
+}
+
+let elUsername = document.getElementById("namaPengguna"); 
+elUsername.addEventListener("keyup", transformUpperCase);
