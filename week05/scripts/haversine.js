e89567a05a06d47825c4d73b5e4adb3e1ac5db31
Sevelen Jaimin <hsweet162@gmail.com>
2020-12-07 21:09:53 +0800

    2109

--- week05/scripts/haversine.js
+++ week05/scripts/haversine.js
@@ -27,7 +27,7 @@ function roundVal(val){
 }
 
 function calculateDistances(lat, lon){
-    let d1 = haversine (lat, lon, 2.922562, 101.650965); //de pulze, cyberjaya
+    let d1 = haversine(lat, lon, 2.922562, 101.650965); //de pulze, cyberjaya
     let d2 = haversine(lat, lon, 3.073065, 101.607787) // sunway pyramid
     let d3 = haversine(lat, lon, 3.158761, 101.714524); //klcc
 
