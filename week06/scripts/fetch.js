7954f72c6ba41cd179c84da20f705e8edb98627d
Sevelen Jaimin <hsweet162@gmail.com>
2020-12-15 20:44:24 +0800

    2044

--- week06/scripts/fetch.js
+++ week06/scripts/fetch.js
@@ -1,10 +1,21 @@
 function GetBookings() {
-    let url = "https://dashboard.sheety.co/projects/5fd8a94536fefc7e9ca3b134/sheets/bookings"
+    let url = 'https://dashboard.sheety.co/projects/5fd8a94536fefc7e9ca3b134/sheets/bookings';
     fetch(url)
         .then((response) => response.json())
         .then(json => {
             //Do somrthing with the data
             console.log(json.bookings);
+            let bookingList = getElementById("bookingList");
+            bookingList.innerHTML = "";
+            for (let i = 0; i < json.bookings.length; i++) {
+                let gName = json.bookings[i].name;
+                let gmail = json.bookings[i].email;
+                let gPax = json.bookings[i].pax;
+                let gId = json.bookings[i].id;
+
+                bookingList.innerHTML += gId + "-" + gName + ", " + gEmail + ", pax:" + "<br>";
+
+            }
         });
 
 
