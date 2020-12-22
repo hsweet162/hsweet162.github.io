8dd9f52977d0e520dcabddfb95399202a902203b
Sevelen Jaimin <hsweet162@gmail.com>
2020-12-22 20:03:09 +0800

    2003

--- week04/scripts.js
+++ week04/scripts.js
@@ -1,41 +1,39 @@
-function transformUpperCase(){
+function transformUpperCase() {
     let a = document.getElementById("namaPengguna");
-    a.value = a.value.toUppercase();
+    a.value = a.value.toUpperCase();
 }
 
-let elUsername = document.getElementById("namaPengguna"); 
+let elUsername = document.getElementById("namaPengguna");
 elUsername.addEventListener("keyup", transformUpperCase);
 
-function checkPasswordLength(){
+function checkPasswordLength() {
     let password = document.getElementById("kataLaluan").value;
-    if(password.length < 5) {
-        alert("Password must be more than 5 characters");
-
-    } else{
-        alert("Password accepted!");
-    }
-}
-
-let elPassword = document.getElementById("kataLaluan");
-elPassword.onblur = checkPasswordLength;
-
-function checkPasswordLength2(txt){
-    if(txt.length < 5) {
+    if (password.length < 5) {
         alert("Password must be more than 5 characters");
     } else {
         alert("Password accepted!")
     }
 }
 
+let elPassword = document.getElementById("kataLaluan");
+elPassword.onblur = checkPasswordLength;
+
+function checkPasswordLength2(txt) {
+    if (txt.length < 5) {
+        alert("Password must be more than 5 characters");
+    } else {
+        alert("Password accepted!");
+    }
+}
+
 let elPassword2 = document.getElementById("kataLaluan2");
-elPassword2.addEventListener("blur", function(){
+elPassword2.addEventListener("blur", function () {
     let a = document.getElementById("kataLaluan2");
     checkPasswordLength2(a.value);
 });
 
-let metallicaLink = document.getElementById("metallicaLink";
-metallicaLink.addEventListener("click", function(){
+let metallicaLink = document.getElementById("metallicaLink");
+metallicaLink.addEventListener("click", function () {
     event.preventDefault();
     alert(metallicaLink.href);
-
-})
+});
